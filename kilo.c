f8a951800dc3bf8b6cb15d6755d796b38732f613
Kris Bolton <krisbolton@users.noreply.github.com>
2021-01-16 13:58:23 +0000

    initial commit
    raw mode flags set. accepts stdin byte-by-byte.

---
+++ kilo.c
@@ -0,0 +1,39 @@
+# include <ctype.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <termios.h>
+# include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+	tcsetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+
+	struct termios raw = orig_termios;
+	raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_lflag &= ~(OPOST);
+	raw.c_lflag &= ~(CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+	enableRawMode();
+
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+	}
+
+	return 0;
+}
