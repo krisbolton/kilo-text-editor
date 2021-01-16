cd677b0e8367907fec73cf8bd42552e00652fa96
Kris Bolton <krisbolton@users.noreply.github.com>
2021-01-16 15:55:42 +0000

    clear screen & reposition cursor on exit
    two exit points, die() and CTRL+Q now clear screen & reposition cursor

--- kilo.c
+++ kilo.c
@@ -1,3 +1,10 @@
+/*
+* Kilo
+* Build Your Own Text Editor
+* Following a tutorial by Pailey (Snaptoken / viewsourcecode.org)
+* https://viewsourcecode.org/snaptoken/kilo/index.html
+*/
+
 # include <ctype.h>
 # include <errno.h>
 # include <stdio.h>
@@ -5,17 +12,27 @@
 # include <termios.h>
 # include <unistd.h>
 
-/*** defines ***/
+/*
+* defines
+*/
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
-/*** data ***/
+/*
+* data
+*/
 
 struct termios orig_termios;
 
-/*** terminal ***/
+/*
+* terminal
+*/
 
 void die(const char *s) {
+	/* Clear screen & reposition cursor*/
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
@@ -50,24 +67,41 @@ char editor_read_key() {
 	}
 }
 
-/*** input ***/
+/*
+* output
+*/
 
+void editor_refresh_screen() {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*
+* input
+*/
 void editor_process_keypress() {
 	char c = editor_read_key();
 
 	switch (c) {
 		case CTRL_KEY('q'):
+		/* Clear screen & reposition cursor*/
+		write(STDOUT_FILENO, "\x1b[2J", 4);
+		write(STDOUT_FILENO, "\x1b[H", 3);
+
 		exit(0);
 		break;
 	}
 }
 
-/*** init ***/
+/*
+* init
+*/
 
 int main() {
 	enable_raw_mode();
 
 	while(1) {
+		editor_refresh_screen();
 		editor_process_keypress();
 	}
 
